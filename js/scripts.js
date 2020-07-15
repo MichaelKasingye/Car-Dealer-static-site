5974f5814208cf6536d17a80422e9f1019f11b81
ek0mike <michaelkasingye@gmail.com>
2020-07-15 04:37:43 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function onSideMenu(){
+document.querySelector('#side-menu')
+.style.width='200px';
+// document.querySelector('.contents')
+// .style.marginLeft='200px';
+}
+
+function offSideMenu(){
+  document.querySelector('#side-menu')
+  .style.width
+  ='0px';
+  document.querySelector('.contents')
+  .style.marginLeft
+  ='0px';
+  }
